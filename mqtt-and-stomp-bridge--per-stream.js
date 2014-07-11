c6e58e8850da670b5265f7bf9c3c7293aab57a69
Luca Capra <luca.capra@gmail.com>
2014-07-11 10:27:07 +0200

    point broker to localhost

--- mqtt-and-stomp-bridge--per-stream.js
+++ mqtt-and-stomp-bridge--per-stream.js
@@ -1,32 +1,36 @@
+
+var host = 'localhost';
+var http_port = '8080';
+
+
 var Stomp = require('stompjs');
 var api = require('restler');
- 
+
 // Use raw TCP sockets
-var client = Stomp.overTCP('api.servioticy.com', 1883);
+var client = Stomp.overTCP(host, 1883);
 // uncomment to print out the STOMP frames
 //client.debug = console.log;
- 
+
 client.connect('compose', 'shines', function(frame) {
     client.subscribe('/topic/*.from', function(message) {
- 
+
         var request = JSON.parse(message.body);
         console.log("Going for a " + request.meta.method);
         console.log("Posted data " + JSON.stringify(request.body));
-        api.json("http://api.servioticy.com" + request.meta.url,
+        api.json("http://" + localhost + ( http_port ? ':'+http_port : '' ) + request.meta.url,
                 request.body,
                 {headers: {'Content-Type': 'application/json', 'Authorization': request.meta.authorization}},
                 request.meta.method
         ).on('complete', function(data, response) {
-            
-    
+
             var headers = {};
-            
+
             // send back the identifier if any, allowing the requesting client to match the response
             // keep in header to not pollute the response body
             if(typeof request.meta.messageId !== 'undefined') {
               headers.messageId = request.meta.messageId
-            }            
-            
+            }
+
             var urlparts = request.meta.url.split("/");
             client.send('/topic/' + request.meta.authorization + ".to", headers, JSON.stringify(data));
             client.send('/topic/' + request.meta.authorization + ".to." + urlparts[urlparts.length-3], headers, JSON.stringify(data));
--- mqtt-and-stomp-bridge.js
+++ mqtt-and-stomp-bridge.js
@@ -1,26 +1,30 @@
+
 var Stomp = require('stompjs');
 var api = require('restler');
- 
+
+var host = 'localhost';
+var http_port = '8080';
+
 // Use raw TCP sockets
-var client = Stomp.overTCP('api.servioticy.com', 1883);
+var client = Stomp.overTCP(host, 1883);
 // uncomment to print out the STOMP frames
 //client.debug = console.log;
- 
+
 client.connect('compose', 'shines', function(frame) {
     client.subscribe('/topic/*.from', function(message) {
- 
+
         var request = JSON.parse(message.body);
         console.log("Going for a " + request.meta.method);
         console.log("Posted data " + JSON.stringify(request.body));
-        api.json("http://api.servioticy.com" + request.meta.url,
+        api.json("http://" + host + ( http_port ? ':'+http_port : '' ) + request.meta.url,
                 request.body,
                 {headers: {'Content-Type': 'application/json', 'Authorization': request.meta.authorization}},
                 request.meta.method
         ).on('complete', function(data, response) {
-            
+
             var headers = {};
-            
-            // send back the identifier if any, allowing the requesting client to match the response        
+
+            // send back the identifier if any, allowing the requesting client to match the response
             // keep in header to not pollute the response body
             if(typeof request.meta.messageId !== 'undefined') {
               headers.messageId = request.meta.messageId
