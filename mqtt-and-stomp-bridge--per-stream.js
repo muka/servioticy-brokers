3ff1230173bdbb899eea7a78fddcc430c469cf6a
Luca Capra <luca.capra@gmail.com>
2015-03-20 09:48:08 +0100

    refactored for better exception control

--- mqtt-and-stomp-bridge--per-stream.js
+++ mqtt-and-stomp-bridge--per-stream.js
@@ -1,41 +1,20 @@
 
-var host = 'localhost';
-var http_port = '8080';
+var start = function(config) {
 
+    var Bridge = require('./lib/StreamBridge');
 
-var Stomp = require('stompjs');
-var api = require('restler');
-
-// Use raw TCP sockets
-var client = Stomp.overTCP(host, 1883);
-// uncomment to print out the STOMP frames
-//client.debug = console.log;
-
-client.connect('compose', 'shines', function(frame) {
-    client.subscribe('/topic/*.from', function(message) {
-
-        var request = JSON.parse(message.body);
-        console.log("Going for a " + request.meta.method);
-        console.log("Posted data " + JSON.stringify(request.body));
-        api.json("http://" + localhost + ( http_port ? ':'+http_port : '' ) + request.meta.url,
-                request.body,
-                {headers: {'Content-Type': 'application/json', 'Authorization': request.meta.authorization}},
-                request.meta.method
-        ).on('complete', function(data, response) {
-
-            var headers = {};
-
-            // send back the identifier if any, allowing the requesting client to match the response
-            // keep in header to not pollute the response body
-            if(typeof request.meta.messageId !== 'undefined') {
-              headers.messageId = request.meta.messageId
-            }
-
-            var urlparts = request.meta.url.split("/");
-            client.send('/topic/' + request.meta.authorization + ".to", headers, JSON.stringify(data));
-            client.send('/topic/' + request.meta.authorization + ".to." + urlparts[urlparts.length-3], headers, JSON.stringify(data));
-            client.send('/topic/' + request.meta.authorization + ".to." + urlparts[urlparts.length-3] + "." + urlparts[urlparts.length-1], headers, JSON.stringify(data));
-            console.log("result: " + JSON.stringify(data));
-        });
+    (new Bridge(config)).start(function() {
+        console.info("Bridge started");
     });
+};
+
+start();
+
+process.on('uncaughtException', function (err) {
+
+    console.error("Uncaught exception detected, exit bridge")
+    console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+    console.error(err.stack);
+
+    process.exit(1);
 });
--- mqtt-and-stomp-bridge.js
+++ mqtt-and-stomp-bridge.js
@@ -1,35 +1,20 @@
-var Stomp = require('stompjs');
-var api = require('restler');
- 
-// Use raw TCP sockets
-var client = Stomp.overTCP('api.servioticy.com', 1883);
-// uncomment to print out the STOMP frames
-//client.debug = console.log;
- 
-client.connect('compose', 'shines', function(frame) {
-    client.subscribe('/topic/*.from', function(message) {
- 
-        var request = JSON.parse(message.body);
-        console.log("Going for a " + request.meta.method);
-        console.log("Posted data " + JSON.stringify(request.body));
-        api.json("http://api.servioticy.com" + request.meta.url,
-                request.body,
-                {headers: {'Content-Type': 'application/json', 'Authorization': request.meta.authorization}},
-                request.meta.method
-        ).on('complete', function(data, response) {
-             // send back the identifier if any, allowing the requesting client to match the response        
-           console.log("input message id: "+request.meta.messageId);
-           response={meta: {}, body:{}};
-           response.meta.messageId = request.meta.messageId || null;
-           response.body = data;
-           console.log("sending: "+JSON.stringify(response));
-           client.send('/topic/'+request.meta.authorization+".to",{},JSON.stringify(response));
-        });
+
+var start = function(config) {
+
+    var Bridge = require('./lib/Bridge');
+    (new Bridge(config)).start(function() {
+        console.info("Bridge started");
     });
-});
+
+};
+
+start();
 
 process.on('uncaughtException', function (err) {
+
+    console.error("Uncaught exception detected, exit bridge")
     console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-    console.error(err.stack)
-    process.exit(1)
-})
+    console.error(err.stack);
+
+    process.exit(1);
+});
