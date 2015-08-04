d97ec00813f29712caec355cf5b6de85b9c7344b
Luca Capra <luca.capra@gmail.com>
2015-08-04 10:30:32 +0200

    added api in index

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
 
-var startBridge = function(config) {
+var Bridge = require('./lib/Bridge');
 
-    var Bridge = require('./lib/Bridge');
-    (new Bridge(config)).start(function() {
-        console.info("So bridge started");
+var instance;
+
+module.exports.start = function(config, then) {
+
+    if(instance) {
+        then && then(instance)
+        return
+    }
+
+    instance = new Bridge(config || {})
+    instance.start(function() {
+        then && then(instance)
     });
+}
 
-};
-
-
-var startStreamBridge = function(config) {
-
-    var Bridge = require('./lib/StreamBridge');
-
-    (new Bridge(config)).start(function() {
-        console.info("Stream bridge started");
-    });
-};
-
-var startActionsBridge = function(config) {
-
-    var Bridge = require('./lib/ActionBridge');
-
-    (new Bridge(config)).start(function() {
-        console.info("Action bridge started");
-    });
-};
-
-
-var start = function() {
-    startBridge();
-//    startStreamBridge();
-//    startActionsBridge();
-};
-
-start();
-
-process.on('uncaughtException', function (err) {
-
-    console.error("Uncaught exception detected, exit bridge")
-    console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-    console.error(err.stack);
-
-    process.exit(1);
-});
+module.exports.stop = function() {
+    if(!instance) return
+    instance.stop();
+    instance = null
+}
